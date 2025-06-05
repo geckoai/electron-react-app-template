19e2e54c43099bf6c7fd9b63ce5b3ba008f9816a
ranyunlong <ranyl@mininglamp.com>
2025-06-05 14:18:49 +0800

    feat: 优化demo

--- src/renderer/application.tsx
+++ src/renderer/application.tsx
@@ -9,15 +9,15 @@ import { I18nService } from '@geckoai/gecko-i18n';
 
 @GeckoModule({ imports: [Router], providers: [I18nService] })
 export class Application {
-  constructor(@inject(ReactRouter.Router) router: RouterProviderProps['router']) {
+  constructor(
+    @inject(ReactRouter.Router) router: RouterProviderProps['router'],
+  ) {
     const root = document.getElementById('root');
     if (root) {
       ReactDOM.createRoot(root).render(
         <StrictMode>
-          <RouterProvider
-            router={router}
-          />
-        </StrictMode>
+          <RouterProvider router={router} />
+        </StrictMode>,
       );
     }
   }
--- src/renderer/home/component.tsx
+++ src/renderer/home/component.tsx
@@ -1,21 +1,23 @@
 import { I18nService, useI18n } from '@geckoai/gecko-i18n';
-import { useContainer } from '@geckoai/gecko-router';
+import { useService } from '@geckoai/gecko-router';
 
 export const Component = () => {
   const i18n = useI18n();
-  const container = useContainer();
 
-  const service = container.get<I18nService>(I18nService);
+  const service = useService<I18nService>(I18nService);
 
-  const [ , setState] = service.asState();
-
+  const [, setState] = service.vm.asState();
 
   return (
     <div>
       {i18n?.Home}
       <div>
-        <button type="button" onClick={() => setState("zh-CN")}>中文</button>
-        <button type="button" onClick={() => setState("en")}>English</button>
+        <button type="button" onClick={() => setState('zh-CN')}>
+          中文
+        </button>
+        <button type="button" onClick={() => setState('en')}>
+          English
+        </button>
       </div>
     </div>
   );
